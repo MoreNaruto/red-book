13232c375a2d267be7b409bcf55f720d72711edb
T-Mo Morris <morenaruto007@gmail.com>
2021-05-30 20:33:05 -0500

    Add first protobuf object

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	elliot := &User{
+		Username: "elliotName",
+		Age:      22,
+		SocialFollowers: &SocialFollowers{
+			Twitter: 1200,
+			Youtube: 2300,
+		},
+	}
+
+	data, err := proto.Marshal(elliot)
+	if err != nil {
+		log.Fatal("Mashalling error: ", err)
+	}
+
+	fmt.Println(data)
+
+	newElliot := &User{}
+	err = proto.Unmarshal(data, newElliot)
+	if err != nil {
+		log.Fatal("UnMashalling error: ", err)
+	}
+
+	fmt.Println(newElliot.GetAge())
+	fmt.Println(newElliot.GetUsername())
+	fmt.Println(newElliot.GetSocialFollowers().GetTwitter())
+	fmt.Println(newElliot.GetSocialFollowers().GetYoutube())
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module cmd/main
+
+go 1.14
+
+require google.golang.org/protobuf v1.26.0
